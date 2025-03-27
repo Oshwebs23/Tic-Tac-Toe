6a5bedc0fcaaff65972e40676dc792639eb09519
Oshwebs23 <osian1996@live.co.uk>
2025-03-27 14:37:38 +0000

    check winner function complete, win conditions set

--- script.js
+++ script.js
@@ -13,38 +13,75 @@ const winConditions = [
 ];
 
 let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "x";
-let running = false;
+let currentPlayer = "X";
+let running = true;
+
 
 beginGame();
+
 
 
 function beginGame () {
     cells.forEach(cell => cell.addEventListener("click", cellClicked))
     restartbutton.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn!`;
+    running = true
 };
 
 function cellClicked () {
     const cellIndex = this.getAttribute("cellIndex");
 
-    if(options[cellIndex] != '' || !running) {
+    if(options[cellIndex] != ""|| !running) {
         return;
     }
 
     updateCell(this, cellIndex);
     checkWinner();
+    //changePlayer(); used to test changePlayer() function 
 };
 
 function updateCell (cell, index) {
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 
 };
 
 function changePlayer () {
-
+    currentPlayer = (currentPlayer == "X" ) ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn!`;
 };
 
 function checkWinner () {
+    let gameWin = false;
+
+    for(let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        } 
+        if(cellA == cellB && cellB == cellC){
+            gameWin = true;
+            break;
+        }
+        
+    }
+
+    if(gameWin){
+        statusText.textContent = `${currentPlayer} has won the game!`;
+        running = false;
+
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
 
 };
 
