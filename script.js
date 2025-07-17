bc98ba8bd367fa9fb783619c58eba70930e2cb20
Oshwebs23 <osian1996@live.co.uk>
2025-07-17 17:21:10 +0100

    fixed typo on line 6.Edited line 41 to remove 'let'. Fixing variable scope issue so now the function resets the game board and not just local copy. Updated the existing global variable

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const statusText = document.querySelector("#status");
 const restartbutton = document.querySelector("#restartButton");
 const winConditions = [
     [0,1,2],
-    [3.4,5],
+    [3,4,5],
     [6,7,8],
     [0,3,6],
     [1,4,7],
@@ -84,7 +84,7 @@ function checkWinner () {
 
 function restartGame () {
     currentPlayer = "X";
-    let options = ["", "", "", "", "", "", "", "", ""];
+    options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
     running = true;
